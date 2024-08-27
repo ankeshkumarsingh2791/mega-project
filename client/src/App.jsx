53e89138c5d869a0a174ab174abe84a1f1cf5316
ankeshkumarsingh2791 <ankeshkumarsingh2791@gmail.com>
2024-08-27 03:35:07 -0700

    store and authslice written

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
 
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwrite/Auth"
 
 
 function App() {
-//  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const {loading, setLoading} = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     
-    <h1>hiii</h1>
-   
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentuser().then((useData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    }).finally( () => setLoading(false))
+  }, [])
+
+  return !loading ? (<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+   <div className='w-full block'>
+       <index />
+   </div>
+  </div>) : null
 }
 
 export default App
---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from './Header'
+import Footer from './Fotter'
+
+export default {
+    Header,
+    Footer
+}
